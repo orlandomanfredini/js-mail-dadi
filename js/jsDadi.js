1bbd030040922f7425a19d6486d595e6639b729e
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-07 16:47:17 +0100

    ADD: numero random da 1 a 6

---
+++ js/jsDadi.js
@@ -0,0 +1,8 @@
+// ---- Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// genero un numero random da 1 a 6 e lo stampo in conole
+
+const dado = Math.floor(Math.random()* 6) + 1; //number
+console.log(dado)
