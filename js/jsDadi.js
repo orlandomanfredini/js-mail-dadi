76b4d9d83b872579b4654c05b09389c58e42f848
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-07 17:40:33 +0100

    ADD: gioco

--- js/jsDadi.js
+++ js/jsDadi.js
@@ -9,12 +9,32 @@ const buttonStart = document.getElementById('btn-start');
 buttonStart.addEventListener('click', function(){
     const player = document.getElementById('number-box');
     const computer = document.getElementById('number-box-computer');
+    const winner = document.getElementById('output-winner')
 
     const dado1 = Math.floor(Math.random()* 6) + 1; //number
     player.innerHTML = dado1
 
     const dado2 = Math.floor(Math.random()* 6) + 1; //number
     computer.innerHTML = dado2
+
+    if (dado1 > dado2){
+        winner.innerHTML = 'Player 1'
+        player.classList.add('text-success')
+        computer.classList.add('text-danger')
+    }else if (dado1 === dado2){
+        winner.innerHTML = 'Pareggio'
+        player.classList.add('text-warning')
+        computer.classList.add('text-warning')
+        
+    }else{
+        winner.innerHTML = 'Computer'
+        player.classList.add('text-danger')
+        computer.classList.add('text-success')
+    }
+
+    winner.classList.add('text-center')
+
+        
 
 
 })
