f9c879367f9919a62ee092f9f545be3f4317f842
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-07 17:18:04 +0100

    ADD: stile

--- js/jsDadi.js
+++ js/jsDadi.js
@@ -2,7 +2,20 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// genero un numero random da 1 a 6 e lo stampo in conole
+// collego bottone al mio js
+const buttonStart = document.getElementById('btn-start');
 
-const dado = Math.floor(Math.random()* 6) + 1; //number
-console.log(dado)
+// quando avviene il click sul button 
+buttonStart.addEventListener('click', function(){
+    const player = document.getElementById('number-box');
+    const computer = document.getElementById('number-box-computer');
+
+    const dado1 = Math.floor(Math.random()* 6) + 1; //number
+    player.innerHTML = dado1
+
+    const dado2 = Math.floor(Math.random()* 6) + 1; //number
+    computer.innerHTML = dado2
+
+
+})
+
