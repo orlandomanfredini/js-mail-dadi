bac7ce057519aba7936f6496c9b53daad8cd2d2f
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-07 16:05:41 +0100

    ADD: js

--- js/jsMail.js
+++ js/jsMail.js
@@ -6,20 +6,32 @@
 // creo collegamento con input e button (del DOM) dove utente inserisce mail
 
 const emailUser = document.getElementById('mail-user'); 
-const buttonCheck = document.getElementById('check-button')
+const buttonCheck = document.getElementById('check-button');
 
 // come richiesto...
  // controllo che utente inserisca mail valida:
  // ascoltando l'evento con il click sul button
 
  buttonCheck.addEventListener('click', function(){
+
+    const esitEmail = document.getElementById('box-mail');
     
     // dopo dichiaro array per lwegittimare le mail
     const listAccess = ['orlando.manfre1@gmail.com', 'lelemora@78.it', 'giovannipaolosecondo@gmail.com', 'cristiano.ronaldo@siuuu.com']
     
     // per poi eseguire la verifica con il ciclo
-    for(let email = ''; email < listAccess.length; email ++ ){
-        console.log(listAccess[email])
+    for(let i = 0; i <= listAccess.length; i ++ ){
+        
+        
+        if (emailUser.value === listAccess[i]){
+            esitEmail.innerHTML = 'Email corretta';
+
+        }else{
+            esitEmail.innerHTML = 'Email errata';
+        }
+
+        
+
     }
 
     //SE mail coincide stampo email corretta
